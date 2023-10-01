35d1b3adc25eff41e634860ed02edfb2f2ab504b
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-10-01 23:35:47 +0800

    modified datetime

--- src/modules/auction/entities/auction.entity.ts
+++ src/modules/auction/entities/auction.entity.ts
@@ -33,8 +33,8 @@ export class Auction {
   @CreateDateColumn()
   created_at?: Date; // Creation date
 
-  @Column({ type: 'datetime' })
-  expiration: Date; // Creation date
+  @Column({ type: 'datetime2' })
+  expiration: Date;
 
   @UpdateDateColumn()
   updated_at?: Date; // Last updated date
