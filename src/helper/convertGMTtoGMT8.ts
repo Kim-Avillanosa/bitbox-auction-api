52ef9a4f432b638d7a54080bcb5764576656d9d3
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-10-05 02:11:46 +0800

    changed date

--- src/helper/convertGMTtoGMT8.ts
+++ src/helper/convertGMTtoGMT8.ts
@@ -5,7 +5,7 @@ export function convertGMTtoGMT8(gmt0Date: Date): Date {
   const gmt0Timestamp = gmt0Date.getTime();
 
   // Calculate the new timestamp with GMT+0800 (UTC+8) offset
-  const gmt8Timestamp = gmt0Timestamp + 8 - 59; // 8 hours in milliseconds
+  const gmt8Timestamp = gmt0Timestamp - 59; // 8 hours in milliseconds
 
   // Create a new Date object with the GMT+0800 timestamp
   const gmt8Date = new Date(gmt8Timestamp);
